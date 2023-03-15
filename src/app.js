190fab695b3761bcc71a0787c042a2ab75d5ed18
mramirez092 <miguelramirez2210@gmail.com>
2023-03-15 18:40:22 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var extension = [".com", ".net", ".io", ".es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomain() {
+  var domainNames = [];
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extension.length; e++) {
+          var domainName = pronoun[p] + adj[a] + noun[n] + extension[e];
+          domainNames.push(domainName);
+        }
+      }
+    }
+  }
+  return domainNames;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(generateDomain());
